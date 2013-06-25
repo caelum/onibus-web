5100ba4411a3ec9848dbea7725f476ff96fdcbce
Sérgio Lopes <sergio.lopes@caelum.com.br>
2013-06-25 18:10:40 -0300

    init project
    (baseado no scafold do yeoman com angular)

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('onibusWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
