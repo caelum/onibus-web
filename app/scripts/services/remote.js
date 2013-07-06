bbdb57ca00c97efad9308caae5cda70ee89acaf4
Sérgio Lopes <sergio.lopes@caelum.com.br>
2013-07-06 01:17:31 -0300

    tenta buscar 5x o serviço antes de explodir exception

--- app/scripts/services/remote.js
+++ app/scripts/services/remote.js
@@ -6,21 +6,38 @@
 'use strict';
 
 window.APP.factory('remote', ['$http', function($http){
+
+  // internal implementation
+  function _remote (url, onsuccess, retries) {
+    var retries = retries || 0;
+
+    $http.jsonp(url).success(onsuccess)
+      .error(function(data, status){
+        if (retries < 5) {
+          _remote(url, onsuccess, ++retries);
+        } else {
+          throw {
+            message: 'Desisti de chamar URL após 5 tentativas.',
+            url: url,
+            status: status,
+            data: data
+          };
+        }
+      });
+  }
+
   return {
 
     pontosProximos: function(position, onsuccess) {
-      var url = 'http://beta.busaosp.com.br/jsonp.php?url=http://ondeestaoalbi2.herokuapp.com/onibusesNosPontosProximos.json&lat='+position.latitude+'&long='+position.longitude+'&callback=JSON_CALLBACK';
-      $http.jsonp(url).success(onsuccess);
+      _remote('http://beta.busaosp.com.br/jsonp.php?url=http://ondeestaoalbi2.herokuapp.com/onibusesNosPontosProximos.json&lat='+position.latitude+'&long='+position.longitude+'&callback=JSON_CALLBACK', onsuccess);
     },
 
     itinerarioLinha: function(idOnibus, onsuccess) {
-      var url = 'http://beta.busaosp.com.br/jsonp.php?url=http://ondeestaoalbi2.herokuapp.com/itinerarioDoOnibus.json&onibus='+idOnibus+'&callback=JSON_CALLBACK';
-      $http.jsonp(url).success(onsuccess);
+      _remote('http://beta.busaosp.com.br/jsonp.php?url=http://ondeestaoalbi2.herokuapp.com/itinerarioDoOnibus.json&onibus='+idOnibus+'&callback=JSON_CALLBACK', onsuccess);
     },
 
     onibusTempoReal: function(codigoGPS, onsuccess) {
-      var url = 'http://beta.busaosp.com.br/jsonp.php?url=http://ondeestaoalbi2.herokuapp.com/localizacoesDoOnibus.json&codigoLinha='+codigoGPS+'&callback=JSON_CALLBACK';
-      $http.jsonp(url).success(onsuccess);
+      _remote('http://beta.busaosp.com.br/jsonp.php?url=http://ondeestaoalbi2.herokuapp.com/localizacoesDoOnibus.json&codigoLinha='+codigoGPS+'&callback=JSON_CALLBACK', onsuccess);
     }
 
   };
