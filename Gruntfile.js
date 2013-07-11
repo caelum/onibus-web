76148c27adeb613fc0c2c32abf93ee09af4ab6c0
Sérgio Lopes <sergio.lopes@caelum.com.br>
2013-07-11 20:18:08 -0300

    bug fix: grunt-rev ignora icones do manifesto do firefox

--- Gruntfile.js
+++ Gruntfile.js
@@ -144,7 +144,8 @@ module.exports = function (grunt) {
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
             '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/styles/fonts/*'
+            '<%= yeoman.dist %>/styles/fonts/*',
+            '!<%= yeoman.dist %>/images/firefox/**/*'
           ]
         }
       }
