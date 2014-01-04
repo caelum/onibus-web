9a2412b24fb34b1d35c94c73ca25bd35bd7094ac
Sérgio Lopes <sergio.lopes@caelum.com.br>
2014-01-04 18:37:10 -0200

    home v0.1

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,3 +5,6 @@ grunt build
 # upload
 echo Upload to server
 rsync -avHz --delete --progress dist/ busaosp@busaosp.com.br:/home/busaosp/beta.busaosp.com.br/
+# upload home
+echo Upload home to server
+rsync -avHz --delete --progress home/ busaosp@busaosp.com.br:/home/busaosp/busaosp.com.br/
